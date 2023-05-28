6158c6ccc1b1cd2fe4b3c0efdbdf889ed03b0bf0
Fis Matt <FisMatt382@gmail.com>
2023-05-28 11:58:24 +0300

    создал файл для ДЗ семинара 3

--- DZS3FisenkoNikolay/DZS3FisenkoNikolay.cs
+++ DZS3FisenkoNikolay/DZS3FisenkoNikolay.cs
@@ -5,3 +5,48 @@
 //12821 -> да
 //23432 -> да
 
+// Console.WriteLine("(Задача 19) Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if ((num%100000==num%10) && (num%10000==num%100))
+// {
+//     Console.WriteLine("Введенное число является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Введеное число не относится к палиндромному");
+// }
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите число");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+double X2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double d =Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+
+Console.WriteLine(d);
+
+//Задача 23
+//Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
